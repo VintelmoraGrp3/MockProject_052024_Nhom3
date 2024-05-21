d61829ac4b619bb98386a3828009825ca97898ee
ngodochanh <qtrung792@gmail.com>
2024-05-21 17:52:48 +0700

    #99 - ngodochanh Implement CustomBanner component and update image display in DefaultBanner

--- src/layout/components/CustomBanner/CustomBanner.jsx
+++ src/layout/components/CustomBanner/CustomBanner.jsx
@@ -1,7 +1,18 @@
 import './CustomBanner.scss';
+import banner from '../../../assets/images/banner.jpg';
 
 function CustomBanner() {
-  return <div>CustomBanner</div>;
+  return (
+    <div className='custom-banner'>
+      <img src={banner} alt='banner' className='custom-banner--img' />
+
+      <div className='custom-banner__content'>
+        <h1 className='custom-banner__content--heading'>
+          SELL YOUR HOME <br /> RIGHT NOW WITH GAYLORD
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default CustomBanner;
--- src/layout/components/DefaultBanner/DefaultBanner.jsx
+++ src/layout/components/DefaultBanner/DefaultBanner.jsx
@@ -1,8 +1,11 @@
 import './DefaultBanner.scss';
+import banner from '../../../assets/images/banner.jpg';
 
 function DefaultBanner() {
   return (
     <div className='default-banner'>
+      <img src={banner} alt='banner' className='default-banner--img' />
+
       <div className='default-banner__content'>
         <h1 className='default-banner__content--heading'>
           AGENT <br /> FRIENDS OF CUSTOMERS
